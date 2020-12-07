b1bcf2386dbaf4fffb9f9bae396e77f57ea56f06
ThomasL <thomaslaure3@gmail.com>
2020-12-07 19:39:46 +0100

    Modify some CSS rules

--- views/head.php
+++ views/head.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="theme-color" content="#ff0000">
+<meta name="theme-color" content="#e30613">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="title" content="Hiroshima Exploration">
 <meta name="author" content="CNAM / Ingemedia">
@@ -11,15 +11,15 @@
 <meta property="og:url" content="">
 <meta property="og:title" content="Hiroshima Exploration">
 <meta property="og:description" content="A tourism website for the city of Hiroshima and the Saigoku road, help to discover the story of the city and the important monuments of the Saigoku road">
-<meta property="og:image" content="">
+<meta property="og:image" content="../assets/images/social_cover.png">
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="">
 <meta property="twitter:title" content="Hiroshima Exploration">
 <meta property="twitter:description" content="A tourism website for the city of Hiroshima and the Saigoku road, help to discover the story of the city and the important monuments of the Saigoku road">
-<meta property="twitter:image" content="">
-<link rel="shortcut icon" href="">
-<link rel="apple-touch-icon" href="">
-<meta name="msapplication-square310x310logo" content="">
+<meta property="twitter:image" content="../assets/images/social_cover.png">
+<link rel="shortcut icon" href="../assets/images/favicon.png">
+<link rel="apple-touch-icon" href="../assets/images/favicon.png">
+<meta name="msapplication-square310x310logo" content="../assets/images/favicon.png">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <link rel="stylesheet" href="../assets/css/style.css">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
